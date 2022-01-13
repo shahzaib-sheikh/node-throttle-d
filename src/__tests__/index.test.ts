50e734ef8c73d74d9494ca6d29d381695a312a95
Shahzaib Sheikh <shahzaib4213@gmail.com>
2022-01-13 15:09:15 +0500

    Added dummy test

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,22 @@
+// var a = require("../index.ts");
+// var redis = require("ioredis");
+
+
+// (async () => {
+//     var b = await a({
+//         ioredis: new redis()
+//     })
+
+//     setInterval(() => {
+//         console.log("asdf")
+//         b.call("a", function () {
+//             console.log("dfsdfasdfdfdsa")
+//         }, 1000
+//         );
+//     }, 100);
+
+// })()
+
+test('Dummy Test', () => {
+    expect("OKAY").toBe('OKAY');
+});
